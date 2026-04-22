f2e89c918f88037be472fa989667716f20b046cf
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-04-22 12:16:16 +0200

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header.jsx";
+import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
@@ -8,7 +8,7 @@ import Contact from "./pages/Contact";
 export default function App() {
   return (
     <Routes>
-      <Route element={<Header />}>
+      <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
