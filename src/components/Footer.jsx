0be9c3ef31c061763450527e55f81b681fcf725d
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-04-22 12:15:28 +0200

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>© 2025 Fake Shop™ — All rights reserved, even though none of this is real.</p>
+        </footer>
+    )
+
+}
