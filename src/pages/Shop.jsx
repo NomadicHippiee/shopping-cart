71b496222852e3fab8b148e1d82c5fbbe17b2656
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-07-04 16:59:34 +0200

    Fix Shop.jsx layout

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,18 @@
+
+import { useEffect } from 'react';
+import { useProducts } from '../context/useProducts';
+
 export default function Shop() {
+    const { products, loading, error } = useProducts();
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
 
     return (
-        <h2>ShopPage</h2>
-    )
+        <div className='product-grid'>
+            {products.map(product => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+    );
 }
