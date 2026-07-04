a2e155b5bb7b07714af4738bc82b2df6679076ca
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-07-04 17:38:44 +0200

    Fix imports

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,19 @@
 
 import { useEffect } from 'react';
 import { useProducts } from '../context/useProducts';
+import { useCart } from '../context/useCart';
+import { ProductCard } from '../components/ProductCard';
 
 export default function Shop() {
     const { products, loading, error } = useProducts();
+    const { dispatch } = useCart();
+
+    const handleAddToCart = (product, quantity) => {
+        dispatch({
+            type: 'ADD_TO_CART',
+            payload: { product, quantity }
+        });
+    };
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
@@ -11,7 +21,7 @@ export default function Shop() {
     return (
         <div className='product-grid'>
             {products.map(product => (
-                <ProductCard key={product.id} product={product} />
+                <ProductCard key={product.id} product={product} onAddToCart={handleAddToCart} />
             ))}
         </div>
     );
