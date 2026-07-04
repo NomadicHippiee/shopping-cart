7f532ff2df7f952cada6a67e97d5e1386eaee30c
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-07-04 16:58:00 +0200

    Change App.jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
 import { CartProvider } from "./context/CartContext.jsx";
+import { ProductsProvider } from "./context/ProductsContext.jsx";
 
 export default function App() {
   return (
-    <CartProvider>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    </Routes>
-    </CartProvider>
+    <ProductsProvider>
+      <CartProvider>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </CartProvider>
+    </ProductsProvider>
   );
 }
-
